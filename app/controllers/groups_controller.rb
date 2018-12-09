c79d3356fed067868c441f3615efba3c5a65eab6
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2018-12-09 16:47:56 +0900

    make create groups function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,22 @@
 class GroupsController < ApplicationController
 
+  def index
+    @messsages = Group.all
+  end
+
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -12,4 +25,8 @@ class GroupsController < ApplicationController
   def update
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
 end
