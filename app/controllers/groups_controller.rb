d25e6dfef1d05b4b8cf610ba967af66d6c0ccf72
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2018-12-10 16:50:19 +0900

    make_sending_messages_function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,6 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
@@ -37,6 +36,6 @@ class GroupsController < ApplicationController
   end
 
   def set_group
-    @group = Group.find(params[:id])
+    @group = Group.find_by(params[:id])
   end
 end
