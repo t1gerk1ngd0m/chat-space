accef21a9b33d73d671b084949beda71815ced3e
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-01-04 18:33:39 +0900

    correct coding

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -21,8 +21,6 @@ $(function(){
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
 
-    console.log(input);
-
     $.ajax({
       type: 'GET',
       url: '/users',
