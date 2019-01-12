9bd507acbd2ecd61d36573206e035a1fc9fa9ac2
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-01-12 13:56:55 +0900

    change factory_girl to factory_bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ RSpec.configure do |config|
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
