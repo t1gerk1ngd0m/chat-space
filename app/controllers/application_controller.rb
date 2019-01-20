eff51a14c4b1722393dce253f8939f979c8ea78b
Taiga Miwa <t1gerk1ngd0m@yahoo.co.jp>
2019-01-20 14:43:47 +0900

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
